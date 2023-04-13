f014f8bda1ad21b924aca1168828b11e06d08673
gozooit <gozoo.it@protonmail.com>
2023-04-13 11:36:28 +0200

    change variable name

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,17 +1,17 @@
 def stock_picker(stock_prices)
-  lower = { day: 0, price: stock_prices[0] }
-  higher = { day: 0, price: stock_prices[0] }
+  lowest = { day: 0, price: stock_prices[0] }
+  highest = { day: 0, price: stock_prices[0] }
   profit = { day: [0, 0], price: 0 }
   stock_prices.each_with_index do |price, day|
-    if price < lower[:price]
-      lower = { day: day, price: price }
-      higher = { day: day, price: price }
-    elsif price > higher[:price]
-      higher = { day: day, price: price }
+    if price < lowest[:price]
+      lowest = { day: day, price: price }
+      highest = { day: day, price: price }
+    elsif price > highest[:price]
+      highest = { day: day, price: price }
     end
 
-    if higher[:price] - lower[:price] > profit[:price]
-      profit = { day: [lower[:day], higher[:day]], price: higher[:price] - lower[:price] } 
+    if highest[:price] - lowest[:price] > profit[:price]
+      profit = { day: [lowest[:day], highest[:day]], price: highest[:price] - lowest[:price] } 
     end
   end
   profit[:day]
